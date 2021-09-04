07e9b40a693627530206f5181b3b3fce85ea68d5
ivandzen <lobodatell@yandex.ru>
2021-09-05 01:11:16 +0300

    kind of a test for de/se right inside main

--- client/main.cpp
+++ client/main.cpp
@@ -1,38 +1,55 @@
-#include <sstream>
 #include <iostream>
+#include <vector>
 
 #include <boost/asio/streambuf.hpp>
 
 #include "../common/PersistableMap.hpp"
 #include "../common/Protocol.hpp"
+#include "../common/Serialization.hpp"
+
+void testCommandMessageDeSerialize()
+{
+    kvdb::CommandMessage comIn{
+        kvdb::CommandMessage::INSERT,
+        "1.THIS\n\tIS A KEY",
+        "2.AND THIS\n\tIS A VALUE",
+    };
+
+    kvdb::CommandMessage comOut;
+
+    boost::asio::streambuf sbuf(1024);
+    std::ostream ostream(&sbuf);
+    std::istream istream(&sbuf);
+
+    static const char scOpen = '(';
+    static const char scDelimiter = ',';
+    static const char scClose = ')';
+
+    std::cout << boost::fusion::tuple_open(scOpen)
+              << boost::fusion::tuple_delimiter(scDelimiter)
+              << boost::fusion::tuple_close(scClose);
+
+    {
+        using namespace kvdb;
+
+        Serialize(comIn, ostream);
+        Deserialize(istream, comOut);
+    }
+
+    std::cout << sbuf.size() << "\n";
+    std::cout << "From sbuf: " << std::string((char*)sbuf.data().begin()->data()) << "\n";
+
+    std::cout << "comIn: " << comIn << "\n";
+    std::cout << "comOut: " << comOut << "\n";
+
+    std::cout << comOut.key << ":" << comOut.value << "\n";
+
+    assert(comIn == comOut);
+}
 
 int main(int argc, char** argv)
 {
-    kvdb::CommandMessage com1{
-        kvdb::CommandMessage::INSERT,
-        "KEY",
-        "VALUE"
-    };
-
-    boost::asio::streambuf sbuf(1024);
-
-    std::ostream ostream(&sbuf);
-    ostream << com1;
-
-    kvdb::CommandMessage com2;
-
-    std::cout << "com2 before = " << com2 << "\n";
-
-    std::istream istream(&sbuf);
-    istream >> com2;
-
-    std::cout << "com1 = " << com1 << "\n";
-    std::cout << "com2 after = " << com2 << "\n";
-    std::cout << "com2 after = " << com2;
-
-    return 0;
-
-    assert(com1 == com2);
+    testCommandMessageDeSerialize();
 
    // kvdb::PersistableMap map("/home/ivan/persistablemap.map");
    // map.Insert("HKJHLKJHLKJHLKJHLK", "KJLKJ:LKJ:LKJ:LK");
