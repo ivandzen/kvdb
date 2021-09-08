45c18c0a97597d6e8db8d98a1586632f000929b8
ivandzen <lobodatell@yandex.ru>
2021-09-08 21:09:02 +0300

    docker deployment and tests

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import os
+import pathlib
 import subprocess
 import random
 
-commandTmp = './build/client/kvdb_cli --host={hostname} --port={port} {query}' 
+commandTmp = '../build/client/kvdb_cli --host={hostname} --port={port} {query}' 
+
+def StartServer():
+   os.system("docker-compose up -d server")
+   
+def StopServer():
+   os.system("docker-compose stop")
 
 class Client:
    def __init__(self, hostname, port):
@@ -39,6 +46,7 @@ class Client:
                               query = query)
       code = os.system(cmd)
       return code == 0
+
       
 symbols = "abcdefghijklmnopqrstuvwxyz1234567890 \t\n_"
       
@@ -50,26 +58,68 @@ def generateRandomString(from_size, to_size):
    return result
 
 if __name__ == "__main__":
+
+   # make sure to run this test from top-level kvdb directory!
+   os.chdir("./docker")
+   StartServer()
+   
    client = Client('localhost', 5001)
-   numRecords = 50
+   numberToInsert = 1000
    maxKeyLength = 1024
    maxValueLength = 1024 * 1024
    compareData = {}
    
-   for i in range(numRecords):
-      key = generateRandomString(500, 1024)
-      value = generateRandomString(10, 50)
+   insertFailed = 0
+   getFailed = 0
+   brokenData = 0
+   deleteFailed = 0
+   
+   ######################################################################
+   # Inserting records
+   for i in range(numberToInsert):
+      key = generateRandomString(500, maxKeyLength)
+      value = generateRandomString(1024 * 40, 1024 * 50)
       if client.Insert(key, value):
          compareData[key] = value
+      else:
+         insertFailed += 1
          
+   ######################################################################
+   # Restarting Server
+   StopServer()
+   StartServer()
+        
+   ######################################################################
+   # Checking records
    for key, value in compareData.items():
       try:
          valueRemote = client.Get(key)
          if value != valueRemote:
-            print(f"Values not compare: {value}\n\n\n{valueRemote}")
+            brokenData += 1
       except e:
-         print("Error occured when querying data from KVDB")
+         getFailed += 1
          
+   ######################################################################
+   # Delete records
    for key, value in compareData.items():
       if not client.Delete(key):
-         print("Failed to delete element")
+         deleteFailed += 1
+         
+   ######################################################################
+   # Print results
+   
+   print(f"Number to insert : {numberToInsert}")
+   print(f"Insert failed : {insertFailed}")
+   print(f"Get failed : {getFailed}")
+   print(f"Broken data : {brokenData}")
+   print(f"Delete failed: {deleteFailed}")
+   
+   StopServer()
+   
+   assert(insertFailed == 0)
+   assert(getFailed == 0)
+   assert(brokenData == 0)
+   assert(deleteFailed == 0)
+   
+   print("ALL TESTS ARE OK")
+         
